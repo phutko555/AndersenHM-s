5698b388a2ad012d04facfcb8004186d07c1fa80
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2025-01-10 01:29:49 +0400

    Added Stream/Lambda Expressions+Optional methods

--- CoWorkingSpace/src/Customer.java
+++ CoWorkingSpace/src/Customer.java
@@ -1,4 +1,5 @@
 import java.io.Serializable;
+import java.util.Optional;
 
 public class Customer implements Serializable {
 
@@ -11,58 +12,33 @@ public class Customer implements Serializable {
         this.workspaceManager = workspaceManager;
     }
     public void browseAvailableSpaces(){
-        boolean available = false;
-        System.out.println("Available spaces: ");
-        for (WorkSpaces workSpaces : workspaceManager.getSpaces()) {
-            if(workSpaces.isAvailabilityStatus()){
-                System.out.println(workSpaces);
-                available = true;
-            }
-        }
-        if(!available){
-            System.out.println("There are no available spaces");
+        long availableCount = workspaceManager.getSpaces().stream().filter(x -> x.isAvailabilityStatus())
+                .peek(x-> System.out.println("Available: " + x)).count();
+        if(availableCount == 0){
+            System.out.println("There aren't any available spaces");
         }
     }
-    public void makeReservation(int id ) throws NotFoundException{
-        WorkSpaces worker = null;
-        for (WorkSpaces workSpaces : workspaceManager.getSpaces()) {
-            if(workSpaces.isAvailabilityStatus() && workSpaces.getId() == id){
-                workSpaces.setAvailabilityStatus(false);
-                worker = workSpaces;
-                break;
-            }
-        }
-        if (worker != null) {
-            reservations.getReservations().add(worker);
+    public void makeReservation(int id) throws NotFoundException{
+        WorkSpaces work = workspaceManager.getSpaces().stream().filter(x -> x.isAvailabilityStatus() && x.getId() == id)
+                .findFirst().orElseThrow(() -> new NotFoundException("No available workspace found with ID " + id));
+
+            work.setAvailabilityStatus(false);
+            reservations.getReservations().add(work);
             System.out.println("Reservation successful for workspace with ID: " + id);
-        } else {
-            throw new NotFoundException("No available workspace found with ID " + id);
-
-        }
-
     }
+
     public void ownReservation(){
-        if (reservations.getReservations().isEmpty()) {
-            System.out.println("You don't have any reservations.");
-        } else {
-            for (WorkSpaces workSpaces : reservations.getReservations()) {
-                System.out.println(workSpaces);
-            }
-        }
+        reservations.getReservations().stream().findFirst().ifPresentOrElse
+                (x -> reservations.getReservations().forEach(System.out::println),
+                () -> System.out.println("You don't have any reservations."));
     }
+
     public void cancelReservation(int id) throws NotFoundException{
-        WorkSpaces worker = null;
-        for(WorkSpaces workSpaces : reservations.getReservations()){
-            if(workSpaces.getId() == id){
-                worker = workSpaces;
-                break;
-            }
-        }
-        if(worker != null){
-            reservations.getReservations().remove(worker.getId());
+        WorkSpaces workSpaces = reservations.getReservations().stream().
+                filter(x -> x.getId() == id).findFirst().
+                orElseThrow(() -> new NotFoundException("No active reservation found with ID " + id));
+
+            reservations.getReservations().remove(workSpaces.getId());
             System.out.println("Reservation with ID: " + id +" Removed Successfully");
-        }else{
-            throw new NotFoundException("No active reservation found with ID " + id);
-        }
     }
 }
