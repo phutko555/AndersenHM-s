ce3dfca0c63995b9e249907778a9299b7ce81088
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2025-01-29 00:53:01 +0400

    created properties file for database configuration

--- CoWorkingSpace/src/utils/DatabaseConnectionManager.java
+++ CoWorkingSpace/src/utils/DatabaseConnectionManager.java
@@ -1,16 +1,31 @@
 package utils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnectionManager {
 
-    private static String url = "jdbc:postgresql://localhost:5432/coworkingspace";
-    private static String user = "postgres";
-    private static String password = "coworkingspace123";
+    private static String url;
+    private static String username;
+    private static String password;
 
-    public static Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(url,user,password);
+    static {
+        try (InputStream input = DatabaseConnectionManager.class.getClassLoader().getResourceAsStream("database.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            url = prop.getProperty("db.url");
+            username = prop.getProperty("db.username");
+            password = prop.getProperty("db.password");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException("Failed to load database properties");
+        }
+    }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
     }
 }
